226a82ac03aac7c9ed85bd35caebc475b9ad69d7
kodladagel <kodladagel@gmail.com>
2021-07-02 15:58:37 +0300

    11. Gün Ödev 2 - FluentValidation

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,28 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator:AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty();
+            RuleFor(c => c.Name).MinimumLength(2);
+            RuleFor(c => c.DailyPrice).NotEmpty();
+            RuleFor(c => c.DailyPrice).GreaterThan(2);
+            //RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(10).When(p => p.CategoryId == 1);
+            //RuleFor(p => p.ProductName).Must(StartWithA).WithMessage("Ürünler A harfi ile başlamalı");
+        }
+
+        private bool StartWithA(string arg)
+        {
+            //a ile başlıyorsa true döner
+            return arg.StartsWith("A");
+        }
+
+    }
+}
