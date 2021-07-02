6d5f041a92b27d1ffb9f229fd83424637cc3c397
kodladagel <kodladagel@gmail.com>
2021-07-02 16:24:43 +0300

    12. Gün Ödev 3 - Aop, Validation Aspect

---
+++ Business/ValidationRules/FluentValidation/RentalValidator.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class RentalValidator : AbstractValidator<Rental>
+    {
+        public RentalValidator()
+        {
+            RuleFor(r => r.CarId).NotEmpty();
+            RuleFor(r => r.CustomerId).NotEmpty();
+            RuleFor(r => r.RentDate).NotEmpty();
+            RuleFor(r => r.ReturnDate).NotEmpty();
+            RuleFor(r => r.RentDate).NotEmpty().GreaterThan(DateTime.Now).WithMessage("Geçmiş tarihte kiralama yapılamaz.");
+            RuleFor(r => r.ReturnDate).GreaterThanOrEqualTo(r => r.RentDate).WithMessage("Aracın teslim tarihi kiralama tarihinden önce olamaz.");
+        }
+    }
+}
---
+++ Core/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Interceptors
+{
+    public abstract class MethodInterceptionBaseAttribute:Attribute, IInterceptor
+    {
+        public int Priority { get; set; }
+
+        public virtual void Intercept(IInvocation invocation)
+    {
+
+    }
+}
+}
