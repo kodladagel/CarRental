c7f7b5eb901d8933f5de7ba006eaf9d7b8f3b82d
kodladagel <kodladagel@gmail.com>
2021-08-08 16:18:21 +0300

    frontend middleware

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -29,7 +29,7 @@ namespace Business.Concrete
         }
 
         [CacheRemoveAspect("ICarService.Get")]
-        [SecuredOperation("car.add,admin")]
+        //[SecuredOperation("car.add,admin")]
         [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
